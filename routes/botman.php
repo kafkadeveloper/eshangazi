d3a8ba6c765603d9d38c020d7aec99e5e077c2f8
Yesaya R. Athuman <yesayaathuman@gmail.com>
2018-03-15 09:48:27 +0000

    Add correct Routes

--- routes/botman.php
+++ routes/botman.php
@@ -17,26 +17,25 @@ $dialogflow = Dialogflow::create(env('DIALOGFLOW_KEY'))->listenForAction();
 
 $botman->middleware->received($dialogflow);
 
-//$botman->hears('maswali', BotManController::class.'@quizConversation');
-$botman->hears('whitelabel.questions', BotManController::class.'@quizConversation')->middleware($dialogflow);
-$botman->hears('whitelabel.fallback', BotManController::class.'@listener')->middleware($dialogflow);
-$botman->hears('whitelabel.greetings.hello', BotManController::class.'@listener')->middleware($dialogflow);
-$botman->hears('whitelabel.greetings.how_are_you', BotManController::class.'@listener')->middleware($dialogflow);
-$botman->hears('whitelabel.greetings.bye', BotManController::class.'@listener')->middleware($dialogflow);
+$botman->hears('eshangazi.questions', BotManController::class.'@quizConversation')->middleware($dialogflow);
+$botman->hears('eshangazi.fallback', BotManController::class.'@listener')->middleware($dialogflow);
+$botman->hears('eshangazi.greetings.hello', BotManController::class.'@listener')->middleware($dialogflow);
+$botman->hears('eshangazi.greetings.how_are_you', BotManController::class.'@listener')->middleware($dialogflow);
+$botman->hears('eshangazi.greetings.bye', BotManController::class.'@listener')->middleware($dialogflow);
 
-$botman->hears('whitelabel.started', MemberController::class.'@started')->middleware($dialogflow);
-$botman->hears('whitelabel.started.yes', MemberController::class.'@store')->middleware($dialogflow);
-$botman->hears('whitelabel.started.no', MemberController::class.'@reject')->middleware($dialogflow);
+$botman->hears('eshangazi.started', MemberController::class.'@started')->middleware($dialogflow);
+$botman->hears('eshangazi.started.yes', MemberController::class.'@store')->middleware($dialogflow);
+$botman->hears('eshangazi.started.no', MemberController::class.'@reject')->middleware($dialogflow);
 
-$botman->hears('whitelabel.item.categories', ItemCategoryController::class.'@showBotMan')->middleware($dialogflow);
-$botman->hears('whitelabel.items', ItemController::class.'@showBotMan')->middleware($dialogflow);
+$botman->hears('eshangazi.item.categories', ItemCategoryController::class.'@showBotMan')->middleware($dialogflow);
+$botman->hears('eshangazi.items', ItemController::class.'@showBotMan')->middleware($dialogflow);
 
-$botman->hears('whitelabel.centers', CenterController::class.'@showBotMan')->middleware($dialogflow);
-$botman->hears('whitelabel.services', ServiceController::class.'@showBotMan')->middleware($dialogflow);
+$botman->hears('eshangazi.centers', CenterController::class.'@showBotMan')->middleware($dialogflow);
+$botman->hears('eshangazi.services', ServiceController::class.'@showBotMan')->middleware($dialogflow);
 
-$botman->hears('whitelabel.experts', PartnerController::class.'@showBotMan')->middleware($dialogflow);
-$botman->hears('whitelabel.experts.district', PartnerController::class.'@showByDistrictBotMan')->middleware($dialogflow);
+$botman->hears('eshangazi.experts', PartnerController::class.'@showBotMan')->middleware($dialogflow);
+$botman->hears('eshangazi.experts.district', PartnerController::class.'@showByDistrictBotMan')->middleware($dialogflow);
 
-$botman->hears('whitelabel.messages', MessageController::class.'@showBotMan')->middleware($dialogflow);
+$botman->hears('eshangazi.messages', MessageController::class.'@showBotMan')->middleware($dialogflow);
 
-$botman->hears('whitelabel.message.details', MessageDetailController::class.'@showBotMan')->middleware($dialogflow);
+$botman->hears('eshangazi.message.details', MessageDetailController::class.'@showBotMan')->middleware($dialogflow);
