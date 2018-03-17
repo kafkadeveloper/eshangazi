7c24c34bf732ccbbe6e08693dc0a9449821f9803
Yesaya R. Athuman <yesayaathuman@gmail.com>
2018-03-17 06:47:08 +0000

    Add Auth Middleware in Currency Controller

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 
 class CurrencyController extends Controller
 {
+    /*
+     * Currency Controller Constructor
+    */
+    public  function  __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
