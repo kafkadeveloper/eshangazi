ab90312a54fd3eeb6fed7ae5b58016e5f0d8bffd
Yesaya R. Athuman <yesayaathuman@gmail.com>
2018-03-30 22:31:08 +0300

    Allow typing with menu

--- config/botman/facebook.php
+++ config/botman/facebook.php
@@ -76,14 +76,14 @@ return [
     'persistent_menu' => [
         [
             'locale' => 'default',
-            'composer_input_disabled' => 'true',
+            'composer_input_disabled' => 'false',
             'call_to_actions' => [
                 [
                     'title' => 'Msaada Zaidi',
                     'type'  => 'nested',
                     'call_to_actions' => [
                         [
-                            'title'     => 'Masaada',
+                            'title'     => 'Msaada',
                             'type'      => 'postback',
                             'payload'   => 'REFERRALS',
                         ],
