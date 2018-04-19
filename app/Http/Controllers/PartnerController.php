e447951c6f67a743fc048cba20ef5d2acca36b3e
Juma Hakika <mohamedhakika@gmail.com>
2018-04-19 09:16:04 +0000

    Showing experts to the user

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -165,14 +165,14 @@ class PartnerController extends Controller
 
         $partners = Partner::where('partner_category_id', 1)->take(5)->get();
 
-        foreach($partners as $partner)
-        {
-            $bot->typesAndWaits(1);
-            $bot->reply($partner->id);
-        }
+        // foreach($partners as $partner)
+        // {
+        //     $bot->typesAndWaits(1);
+        //     $bot->reply($partner->id);
+        // }
             
 
-        //$bot->reply($this->partners($partners));
+        $bot->reply($this->partners($partners));
 
         //$this->getNearExperts($bot, $partners, $user, $member);
     }
