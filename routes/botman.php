2e32a40397bb1bdf39f41d1d732a2393e82e5c2f
Juma Hakika <mohamedhakika@gmail.com>
2018-04-23 08:26:32 +0000

    Debugging new user conversation

--- routes/botman.php
+++ routes/botman.php
@@ -40,7 +40,7 @@ $dialogflow = Dialogflow::create(env('DIALOGFLOW_KEY'))->listenForAction();
 $botman->middleware->received($dialogflow);
 
 $botman->hears(env('APP_ACTION') . '.questions', BotManController::class.'@quizConversation')->middleware($dialogflow);
-//$botman->hears(env('APP_ACTION') . '.fallback', BotManController::class.'@listener')->middleware($dialogflow);
+$botman->hears(env('APP_ACTION') . '.fallback', BotManController::class.'@listener')->middleware($dialogflow);
 $botman->hears(env('APP_ACTION') . '.help', BotManController::class.'@listener')->middleware($dialogflow);
 $botman->hears(env('APP_ACTION') . '.greetings.hello', BotManController::class.'@listener')->middleware($dialogflow);
 $botman->hears(env('APP_ACTION') . '.greetings.how_are_you', BotManController::class.'@listener')->middleware($dialogflow);
